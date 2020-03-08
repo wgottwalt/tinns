f3a817c34acd7c9f3e76ed4b892c9e480dc47a93
Wilken 'Akiko' Gottwalt <12194808+wgottwalt@users.noreply.github.com>
2020-03-08 19:09:24 +0100

    common time namespace cleanup
    Changed all the time namespace functions to constexpr templates and
    changed the makefiles.

--- tinns/common/Time.hxx
+++ tinns/common/Time.hxx
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <chrono>
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 namespace Time
 {
     //--- types and constants ---
+
     using Clock = std::chrono::system_clock;
     using Rep = Clock::rep;
     using Period = Clock::period;
@@ -13,16 +16,52 @@ namespace Time
     using Timepoint = Clock::time_point;
 
     // fromat: YYYYMMDD.hhmmss - 24h
+
     static const std::string DefCompactDateTime = "%Y%m%j.%H%M%S";
     static const std::string DefDateTime = "%Y/%m/%j %H:%M:%S";
 
     //--- functions ---
-    Timepoint now();
-    time_t nowTimeT();
 
-    time_t toTimeT(const Timepoint &timepoint);
-    Timepoint toTimepoint(const time_t timet);
+    template <typename T = Clock>
+    constexpr Timepoint now()
+    {
+        return T::now();
+    }
 
-    std::string toDateString(const time_t timet, const std::string &format = DefCompactDateTime);
-    std::string toDateString(const Timepoint &timepoint, const std::string &format = DefCompactDateTime);
+    template <typename T = Clock>
+    constexpr time_t toTimeT(const Timepoint &timepoint)
+    {
+        return T::to_time_t(timepoint);
+    }
+
+    template <typename T = Clock>
+    constexpr Timepoint toTimepoint(const time_t timet)
+    {
+        return T::from_time_t(timet);
+    }
+
+    template <typename T = Clock>
+    constexpr time_t nowTimeT()
+    {
+        return toTimeT<T>(now<T>());
+    }
+
+    template <typename T = Clock>
+    constexpr std::string toDateString(const time_t timet,
+                                       const std::string &format = DefCompactDateTime)
+    {
+        const struct std::tm temp = *std::localtime(&timet);
+        std::stringstream result;
+
+        result << std::put_time(&temp, format.c_str());
+
+        return result.str();
+    }
+
+    template <typename T = Clock>
+    constexpr std::string toDateString(const typename T::time_point &timepoint,
+                                       const std::string &format = DefCompactDateTime)
+    {
+        return toDateString(toTimeT(timepoint), format);
+    }
 }
